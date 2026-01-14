883bb017ff817f05d111a73faabe241deb789d60
Gabriela Araujo <gabyara237@gmail.com>
2026-01-13 19:49:42 -0800

    feat: add mailbox components and configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router';
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails"
 import MailboxList from "./components/MailboxList/MailboxList"
+import MailboxForm from "./components/MailboxForm/MailboxForm"
 import NavBar from "./components/NavBar/NavBar"
 
 import './App.css'
@@ -11,6 +13,8 @@ const App = () =>{
       <Routes>
         <Route path='/' element={<h2>Home Page</h2>}/>
         <Route path='/mailboxes' element={<MailboxList/>}/>
+        <Route path='/new-mailbox' element={<MailboxForm/>}/>
+        <Route path='/mailboxes/:mailboxId' element={<MailboxDetails/>}/>
       </Routes>
     </>
   )
